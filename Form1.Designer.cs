a7be2a5c7486981bfc64b5a3d5a9701d501fd9b1
tdzd1 <zhenyako4nev@gmail.com>
2024-09-22 14:19:28 +0500

    появился текстбокс "Укажите кол-во знаков полсе запятой"

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -56,6 +56,8 @@
             this.button18 = new System.Windows.Forms.Button();
             this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
             this.button19 = new System.Windows.Forms.Button();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
             this.SuspendLayout();
@@ -105,6 +107,7 @@
             // 
             this.contextMenuStrip1.Name = "contextMenuStrip1";
             this.contextMenuStrip1.Size = new System.Drawing.Size(61, 4);
+            this.contextMenuStrip1.Opening += new System.ComponentModel.CancelEventHandler(this.contextMenuStrip1_Opening);
             // 
             // label2
             // 
@@ -159,6 +162,7 @@
             this.textBox1.Size = new System.Drawing.Size(238, 49);
             this.textBox1.TabIndex = 15;
             this.textBox1.TextChanged += new System.EventHandler(this.textBox1_TextChanged_1);
+            this.textBox1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox1_KeyPress);
             // 
             // textBox3
             // 
@@ -319,12 +323,34 @@
             this.button19.UseVisualStyleBackColor = true;
             this.button19.Click += new System.EventHandler(this.button19_Click);
             // 
+            // textBox2
+            // 
+            this.textBox2.BackColor = System.Drawing.Color.WhiteSmoke;
+            this.textBox2.Location = new System.Drawing.Point(708, 258);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(24, 23);
+            this.textBox2.TabIndex = 30;
+            this.textBox2.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox2_KeyPress);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Tai Le", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(497, 261);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(205, 14);
+            this.label1.TabIndex = 31;
+            this.label1.Text = "Укажите кол-во знаков полсе запятой";
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 14F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.BackColor = System.Drawing.Color.IndianRed;
             this.ClientSize = new System.Drawing.Size(790, 503);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox2);
             this.Controls.Add(this.button19);
             this.Controls.Add(this.button18);
             this.Controls.Add(this.button17);
@@ -389,6 +415,8 @@
         private System.Windows.Forms.Button button18;
         private System.Windows.Forms.ErrorProvider errorProvider1;
         private System.Windows.Forms.Button button19;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox2;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -48,106 +48,104 @@ namespace калькулятор
         {
 
         }
-        private void button5_Click(object sender, EventArgs e)
+        private void button5_Click(object sender, EventArgs e)//При нажатии на кнопку 5(Посчитать)
         {
-            double chislo;
-            double.TryParse(textBox1.Text, out chislo);
-            
-            if (chislo >= 0  )
+            double chislo = Convert.ToDouble(textBox1.Text);//Строку из поля ввода конвертируем в тип double
+            int kolvo_znakov=Convert.ToInt32(textBox2.Text);//Строку из поля ввода кол-ва знаков полсе запятой конвертируем в тип int
+
+            if (chislo >= 0)//Если число больше либо равно 0
             {
-                double result = Math.Sqrt(chislo * 1.0);
-                textBox3.Text = "+-" + result.ToString();
-               
+                double result = Math.Round(Math.Sqrt(chislo * 1.0), kolvo_znakov);//То в результат присваиваем квадратный корень числа с заданным кол-вом знаков полсе запятой
+                textBox3.Text = "+-" + result.ToString();//В поле поле для вывода выводится +- и значение корня
+
             }
-            else
+            else//если число меньше 0
             {
-                double result = Math.Sqrt(Math.Abs(chislo * 1.0));
-                textBox3.Text = "+-"+ result.ToString()+'i';
+                double result = Math.Round(Math.Sqrt(Math.Abs(chislo * 1.0)), kolvo_znakov);//То в результат присваиваем квадратный корень модуля числа с заданным кол-вом знаков полсе запятой
+                textBox3.Text = "+-" + result.ToString() + 'i';//В поле поле для вывода выводится +- и значение корня
             }
 
         }
-
         private void textBox1_TextChanged_1(object sender, EventArgs e)
         {
-         
+        
+        }
+        private void button10_Click(object sender, EventArgs e)//При нажатии на кнопку 10(5)
+        {
+            textBox1.Text = textBox1.Text + 5;//В поле ввода вводится цифра 5
         }
 
-        private void button10_Click(object sender, EventArgs e)
+        private void button11_Click(object sender, EventArgs e)//При нажатии на кнопку 11(6)
         {
-            textBox1.Text = textBox1.Text + 5;
+            textBox1.Text = textBox1.Text + 6;//В поле ввода вводится цифра 6
         }
 
-        private void button11_Click(object sender, EventArgs e)
+        private void button13_Click(object sender, EventArgs e)//При нажатии на кнопку 13(2)
         {
-            textBox1.Text = textBox1.Text + 6;
+            textBox1.Text = textBox1.Text + 2;//В поле ввода вводится цифра 2
         }
 
-        private void button13_Click(object sender, EventArgs e)
+        private void button6_Click(object sender, EventArgs e)//При нажатии на кнопку 6(7)
         {
-            textBox1.Text = textBox1.Text + 2;
+            textBox1.Text = textBox1.Text + 7;//В поле ввода вводится цифра 7
         }
 
-        private void button6_Click(object sender, EventArgs e)
+        private void button7_Click(object sender, EventArgs e)//При нажатии на кнопку 7(8)
         {
-            textBox1.Text = textBox1.Text + 7;
+            textBox1.Text = textBox1.Text + 8;//В поле ввода вводится цифра 8
         }
 
-        private void button7_Click(object sender, EventArgs e)
+        private void button8_Click(object sender, EventArgs e)//При нажатии на кнопку 8(9)
         {
-            textBox1.Text = textBox1.Text + 8;
+            textBox1.Text = textBox1.Text + 9;//В поле ввода вводится цифра 9
         }
 
-        private void button8_Click(object sender, EventArgs e)
+        private void button9_Click(object sender, EventArgs e)//При нажатии на кнопку 9(4)
         {
-            textBox1.Text = textBox1.Text + 9;
+            textBox1.Text = textBox1.Text + 4;//В поле ввода вводится цифра 4
         }
 
-        private void button9_Click(object sender, EventArgs e)
+        private void button12_Click(object sender, EventArgs e)//При нажатии на кнопку 12(1)
         {
-            textBox1.Text = textBox1.Text + 4;
+            textBox1.Text = textBox1.Text + 1;//В поле ввода вводится цифра 1
         }
 
-        private void button12_Click(object sender, EventArgs e)
+        private void button14_Click(object sender, EventArgs e)//При нажатии на кнопку 14(3)
         {
-            textBox1.Text = textBox1.Text + 1;
+            textBox1.Text = textBox1.Text + 3;//В поле ввода вводится цифра 3
         }
 
-        private void button14_Click(object sender, EventArgs e)
+        private void button15_Click(object sender, EventArgs e)//При нажатии на кнопку 15(0)
         {
-            textBox1.Text = textBox1.Text + 3;
+            textBox1.Text = textBox1.Text + 0;//В поле ввода вводится цифра 0
         }
 
-        private void button15_Click(object sender, EventArgs e)
+        private void button16_Click(object sender, EventArgs e)//При нажатии на кнопку 16(.)
         {
-            textBox1.Text = textBox1.Text + 0;
+            textBox1.Text = textBox1.Text + '.';//В поле ввода вводится .
         }
 
-        private void button16_Click(object sender, EventArgs e)
+        private void button17_Click(object sender, EventArgs e)//При нажатии на кнопку 17(-)
         {
-            textBox1.Text = textBox1.Text + '.';
+            textBox1.Text = textBox1.Text + '-'; //В поле ввода вводится -
         }
 
-        private void button17_Click(object sender, EventArgs e)
+        private void button18_Click(object sender, EventArgs e)//При нажатии на кнопку 18(del)
         {
-            textBox1.Text = textBox1.Text + '-';
-        }
-
-        private void button18_Click(object sender, EventArgs e)
-        {
-            int lenght = textBox1.Text.Length - 1;
-            string text = textBox1.Text;
-            textBox1.Clear();
+            int lenght = textBox1.Text.Length - 1;//Вводим новую переменную lenght целочисленного типа и записываем в неё количество символов в TextBox’e минус один символ.
+            string text = textBox1.Text;//Также мы вводим новую переменную text, в которую полностью заносим текст из TextBox’а
+            textBox1.Clear();//Очищаем TextBox
             for (int i = 0; i < lenght; i++)
             {
-                textBox1.Text = textBox1.Text + text[i];
+                textBox1.Text = textBox1.Text + text[i];//Записываем в TextBox строку text, но уже на символ короче
             }
         }
 
         private void textBox3_TextChanged(object sender, EventArgs e)
         {
-            if (textBox3.Text == "+-0")
+            if (textBox3.Text == "+-0")//Если в поле вывода строка равна +-0
             {
-                textBox3.Text = " ";
+                textBox3.Text = "0";//То заменяем её на строку 0
             }
         }
 
@@ -159,6 +157,34 @@ namespace калькулятор
         private void button3_Click(object sender, EventArgs e)
         {
 
+        }
+
+        private void contextMenuStrip1_Opening(object sender, CancelEventArgs e)
+        {
+
+        }
+
+        private void maskedTextBox1_MaskInputRejected(object sender, MaskInputRejectedEventArgs e)
+        {
+
+        }
+
+        private void textBox1_KeyPress(object sender, KeyPressEventArgs e)//Когда пользователь нажимает любую кнопку в поле для введение подкоренного выражения
+        {
+            char number = e.KeyChar;//Объявляем символьную переменную под названием char
+            if (!Char.IsDigit(number) && number != 8 && number != 46 && number != 45) //Если символ из переменной number не относится к категории десятичных цифр или клавише BackSpace или точку или минусу
+            {
+                e.Handled = true;//Тогда не обрабатывать введенный символ
+            }
+        }
+
+        private void textBox2_KeyPress(object sender, KeyPressEventArgs e)//Когда пользователь нажимает любую кнопку в поле для введения кол-ва знаков полсе запятой
+        {
+            char number = e.KeyChar;//Объявляем символьную переменную под названием char
+            if (!Char.IsDigit(number) && number != 8) //Если символ из переменной number не относится к категории десятичных цифр или клавише BackSpace
+            {
+                e.Handled = true;//Тогда не обрабатывать введенный символ
+            }
         }
     }
 }
